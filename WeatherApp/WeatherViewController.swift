2e3367d0e252657a44efb6110c2cc5523061d31e
Arsalan Balzhinimaev <arsalashka@gmail.com>
2024-05-07 13:51:32 -0500

    init

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+#Preview {
+    WeatherViewController()
+}
