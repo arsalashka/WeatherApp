ed09f7bb03dd25e9d7cc15ccb4fc14ecb5a860c1
Arsalan Balzhinimaev <arsalashka@gmail.com>
2024-06-09 15:21:02 -0400

    add WeatherProvider

--- WeatherApp/Application/SceneDelegate.swift
+++ WeatherApp/Application/SceneDelegate.swift
@@ -10,14 +10,24 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
+    private let weatherProvider = WeatherProviderImpl()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         let viewController = CitySelectionViewController()
-        viewController.viewModel = CitySelectionViewModel()
+        viewController.viewModel = CitySelectionViewModel(weatherProvider: weatherProvider)
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = UINavigationController(rootViewController: viewController)
         window?.makeKeyAndVisible()
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        weatherProvider.sceneWillEnterForeground()
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        weatherProvider.sceneDidEnterBackground()
     }
 }
